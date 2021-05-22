5cead646289d21c6aa9aa5578bbd29ec23a45ec9
Abhav <thakur.abhav25@gmail.com>
2021-05-22 13:58:07 +0530

    login screen

--- app/screens/student/Details.js
+++ app/screens/student/Details.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, TextInput } from 'react-native';
+import {View, StyleSheet, Text, Image, TextInput} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-function Details({ route }) {
+function Details({route}) {
   const listening = route.params;
   return (
     <View style={styles.container}>
@@ -18,9 +18,7 @@ function Details({ route }) {
         resizeMode="contain"
       />
       <View style={styles.box}>
-        <Text style={styles.sub}>
-          {listening.post}
-        </Text>
+        <Text style={styles.sub}>{listening.post}</Text>
       </View>
       <View style={styles.box2}>
         <Text style={styles.sub2}>{listening.mail}</Text>
@@ -30,7 +28,7 @@ function Details({ route }) {
           placeholderTextColor="black"
           placeholder="Give feedback"
           textAlign="left"
-          style={{ fontSize: 14, marginLeft: 10, fontWeight: "bold" }}
+          style={{fontSize: 14, marginLeft: 10, fontWeight: 'bold'}}
         />
       </View>
     </View>
@@ -44,7 +42,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    height: hp("40%"),
+    height: hp('40%'),
   },
   txt: {
     color: 'black',
@@ -58,46 +56,42 @@ const styles = StyleSheet.create({
     fontSize: 23,
     marginLeft: wp('8%'),
     marginTop: hp('2%'),
-    color: "white",
-    fontWeight: "bold"
-
+    color: 'white',
+    fontWeight: 'bold',
   },
   sub2: {
     color: 'white',
     fontSize: 20,
     marginLeft: wp('40%'),
-
   },
   box: {
     height: hp('10%'),
-    width: wp("100%"),
-    marginTop: hp("3%"),
-    backgroundColor: "#e44e73",
-    fontWeight: "bold",
+    width: wp('100%'),
+    marginTop: hp('3%'),
+    backgroundColor: '#e44e73',
+    fontWeight: 'bold',
     // borderRadius: wp("100%") / 28,
-    alignItems: "center",
-
+    alignItems: 'center',
   },
   box2: {
     height: hp('10%'),
-    width: wp("100%"),
+    width: wp('100%'),
     // marginTop: hp("3%"),
-    backgroundColor: "#f57595",
-    fontWeight: "bold",
+    backgroundColor: '#f57595',
+    fontWeight: 'bold',
     paddingTop: 20,
-
   },
   box3: {
     height: hp('20%'),
-    width: wp("80%"),
-    marginTop: hp("3%"),
-    backgroundColor: "#ebf3f4",
-    fontWeight: "bold",
-    borderRadius: wp("100%") / 28,
-    marginLeft: wp("10%"),
+    width: wp('80%'),
+    marginTop: hp('3%'),
+    backgroundColor: '#ebf3f4',
+    fontWeight: 'bold',
+    borderRadius: wp('100%') / 28,
+    marginLeft: wp('10%'),
     paddingTop: 20,
     elevation: 3,
-  }
+  },
 });
 
 export default Details;
